ab69112138d888890322cb91b089716151475162
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2024-02-05 20:01:42 +0800

    Added geopanda with the amenities

---
+++ python geopanda/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,4 @@
+import geopandas as gpd
+
+cities = gpd.read_file('C:/Users/almon/OneDrive/Desktop/python geopanda/geofabrik philippines/gis_osm_buildings_a_free_1.shp')
+
---
+++ python geopanda/main.py
@@ -0,0 +1,5 @@
+import geopandas as gpd
+
+cities = gpd.read_file('gis_osm_buildings_a_free_1.shp')
+
+print("Went here")
