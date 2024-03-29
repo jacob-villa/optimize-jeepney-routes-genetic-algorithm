a9cb617b440598477833643226ef3df1b8052a55
Jacob Miguel Villa <jacob_villa@dlsu.edu.ph>
2024-03-30 04:26:19 +0800

    update: full GA logic with crossovers

--- networkbuilder/genetic_algorithm.py
+++ networkbuilder/genetic_algorithm.py
@@ -10,53 +10,97 @@ num_evolutions -> num_generations
 num_generated_network_mutations_per_evolution -> num_mutations_per_generation
 mutation_probabilities = list of probabilities for mutation that will be randomly selected from
     e.g. [0.1, 0.2, 0.3, 0.4, 0.5] would mean a 0.1 probability for 0 mutations, 0.2 probability for 1 mutation, etc
-
 """
+
+# This implementation takes only 2 parents from the whole generation and generates the population from them
+# Instead of the what's in the paper that says the whole population will go through crossovers and mutations
 # Cite Nayeem et al for GA with elitism and growing population size
-def perform_genetic_algorithm(network_population, num_elites, num_generations, mutation_probabilities, crossover_probabilities,
+def perform_genetic_algorithm(network_population, population_size, num_elites, num_generations, mutation_probability, num_mutations_probabilities, num_crossovers_probabilities,
                               with_elitism=False, with_growing_population=False, num_mutations_per_generation=1):
-    new_network_population = []
+    
     # Do this for the assigned number of generations for the GA
     for i in range(num_generations):
+
+        new_network_population = []
+
         # Evaluate the fitness of each network in the population
         for network in network_population:
             network.fitness_score = compute_fitness_score(network)
 
         sorted_network_population = sorted(network_population, key=lambda x: x.fitness_score, reverse=True)
+        
+        # Most naive selection approach: get top two scoring networks as parents
+        # But should be random with weighted probabilities so that elites are not always parents
+        parent1 = sorted_network_population[0]
+        parent2 = sorted_network_population[1]
 
         # Take num_elites number of the best networks and automatically add them to the next generation
         if (with_elitism):
             for i in range(num_elites):
                 new_network_population.append(sorted_network_population[i])
 
-        # Randomly select how many crossovers for this generation
-        num_crossovers = np.random.choice(len(crossover_probabilities), 1, p=crossover_probabilities)[0]
+        # Ex: population_size = 20 and num_elites = 2
+        # If no elitism and no growing population, then we will have 10 iterations to produce 20 in the next generation
+        # Also, if elitism and growing population, then we will have 10 iterations to produce 22 in the next generation
+        if (not with_elitism and not with_growing_population or with_elitism and with_growing_population):
+            num_iterations = population_size / 2
 
-        # Perform all crossovers 
-        for i in range(num_crossovers):
-            # Randomly select two networks from the population
-            network1 = random.choice(network_population)
-            network2 = random.choice(network_population)
+        # If with elitism only, maintain the population size and account for the already added elites
+        elif (with_elitism):  
+            num_iterations = (population_size - num_elites) / 2
 
-            # Perform crossover on the two networks
-            network1, network2 = crossover(network1, network2)
+        # Generate the population
+        for i in range(num_iterations):
+            # Get 2 children from crossovers between the two parents
+            child1, child2 = crossover_split_index(parent1, parent2)
+            #child1, child2 = crossover_swap_routes(parent1, parent2, num_crossovers_probabilities)
 
-        # Perform mutations
-        for network in network_population:
-            # Randomly select how many mutations for this network
-            # This should be more heavily weighted towards 0 mutations
-            num_mutations = np.random.choice(len(mutation_probabilities), 1, p=mutation_probabilities)[0]
+            num_mutations = np.random.choice(len(num_mutations_probabilities), 1, p=num_mutations_probabilities)[0]
 
-            # Perform all mutations
-            if num_mutations > 0:
-                for i in range(num_mutations):
-                    network = mutate(network)
-    
-    return new_network_population
+            for j in range(num_mutations):
+                # Apply mutations to the children based on mutation probability hyperparameter
+                if np.random.rand() < mutation_probability:
+                    child1 = mutate(child1)
+                if np.random.rand() < mutation_probability:
+                    child2 = mutate(child2)
+            
+            # Add the children to the new population
+            new_network_population.append(child1)
+            new_network_population.append(child2)
 
+            """
+            weird stuff here dont mind
+            # Randomly select how many crossovers for this generation
+            num_crossovers = np.random.choice(len(crossover_probabilities), 1, p=crossover_probabilities)[0]
 
+            # Perform all crossovers 
+            for i in range(num_crossovers):
+                # Randomly select two networks from the population
+                network1 = random.choice(network_population)
+                network2 = random.choice(network_population)
+
+                # Perform crossover on the two networks
+                network1, network2 = crossover(network1, network2)
+
+            # Perform mutations
+            for network in network_population:
+                # Randomly select how many mutations for this network
+                # This should be more heavily weighted towards 0 mutations
+                num_mutations = np.random.choice(len(mutation_probabilities), 1, p=mutation_probabilities)[0]
+
+                # Perform all mutations
+                if num_mutations > 0:
+                    for i in range(num_mutations):
+                        network = mutate(network)
+            """
+        # Assign to next generation
+        network_population = new_network_population
+
+    return network_population
+
+# This crossover implementation splits both networks at an index and exchanges halves
 # Assumes that ideally both networks have the same number of routes (same length)
-def crossover(network1, network2):
+def crossover_split_index(network1, network2):
     # Split both networks at a random index
     if len(network1) < len(network2):
         split_index = random.randint(0, len(network2))
@@ -74,12 +118,33 @@ def crossover(network1, network2):
 
     return network1, network2
 
+# This crossover implementation randomly selects a route from each network and swaps them
+# More similar to the previous thesis implementation
+# num_crossovers_probabilities = list of probabilities for crossovers that will be randomly selected from
+#    e.g. [0.1, 0.2, 0.3, 0.4, 0.5] would mean a 0.1 probability for 0 crossovers, 0.2 probability for 1 crossover, etc
+def crossover_swap_routes(network1, network2, num_crossovers_probabilities):
+    num_crossovers = np.random.choice(len(crossover_probabilities), 1, p=crossover_probabilities)[0]
+
+    for i in range(num_crossovers):
+        # Randomly select a route from each network
+        route1 = random.choice(network1.items())
+        route2 = random.choice(network2.items())
+
+        # Swap the routes
+        network1[route1[0]] = route2[1]
+        network2[route2[0]] = route1[1]
+
+    return network1, network2
+
+
 # Modify the stop connections of a random route in the network
+# Can't really implement this without the data structure of the network
 def mutate(network):
-    selected_route = random.choice(network.items())
+    pass
+
 
 """
-This was generated by copilot
+COPILOT GENERATED CODE
 def perform_genetic_algorithm(population_size, generations, mutation_rate, crossover_rate, selection_rate, fitness_function, crossover_function, mutation_function, selection_function, initial_population=None, verbose=False):
     
     Perform a genetic algorithm on a population of networks.
